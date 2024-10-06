027dc60239b3c2e7280f576ae086e17ee4fc12f6
shaon07 <shamirul@softic.ai>
2024-10-06 13:57:08 +0600

    get ip

--- src/app/api/ip/route.ts
+++ src/app/api/ip/route.ts
@@ -1,10 +1,11 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: Request) {
+export async function GET(req: NextRequest) {
   // Extract the 'x-forwarded-for' header
   const forwardedFor = req.headers.get("x-forwarded-for");
-  // Split to handle cases where multiple proxies are used
+  // Split to handle multiple proxies
   const ip = forwardedFor?.split(",")[0] || "Unknown IP";
 
-  return NextResponse.json({ msg: `Hello from server x ${ip}` });
+  // Return the response as a JSON object
+  return NextResponse.json({ ip });
 }
